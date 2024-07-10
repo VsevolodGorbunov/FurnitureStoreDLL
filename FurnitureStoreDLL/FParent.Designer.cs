55fe84c9f1b33caa3311efbb0391bf346c3dc264
Vsevolod <gorbunovvo@mer.ci.nsu.ru>
2024-07-10 14:25:50 +0700

    Init commit

---
+++ FurnitureStoreDLL/FParent.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace FurnitureStoreDLL
+{
+    partial class FParent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FParent));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FurnitureStoreDLL.Properties.Resources._34822720;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 25);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(108, 110);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FParent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FurnitureStoreDLL.Properties.Resources.Слой_1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(656, 601);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
+            this.Name = "FParent";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FParent";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ FurnitureStoreDLL/FParent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FurnitureStoreDLL
+{
+    public partial class FParent : Form
+    {
+        [DllImport("Gdi32.dll")]
+        public static extern IntPtr CreateRoundRectRgn(int nLeftRect,
+                                                       int nTopRect,
+                                                       int nRightRect,
+                                                       int nBottomRect,
+                                                       int nWidthEllipse,
+                                                       int nHeightEllipse);
+        [DllImport("user32.dll")]
+
+        public static extern int SetWindowRgn(IntPtr hWnd, IntPtr hRgn, bool bRedraw);
+        public FParent()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(FParent_Load);
+        }
+
+        private void FParent_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+            path.AddEllipse(0, 0, 95, 95);
+            Region rgn = new Region(path);
+            pictureBox1.Region = rgn;
+            pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+        }
+    }
+}
